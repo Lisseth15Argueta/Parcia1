1ecfea16352b6a02dc4ddf22253df0b9e473dd9b
Lisseth <arguetalisseth029@gmail.com>
2025-03-07 00:36:23 -0600

    Agregar archivos de proyecto.

---
+++ Parcia1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcia1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
